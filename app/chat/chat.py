f6d399909a89262aa48c3574a383677470bac64c
Marcus Martinez <gmarcusmartinez@gmail.com>
2024-04-11 11:20:45 +0200

    streaming complete

--- app/chat/chat.py
+++ app/chat/chat.py
@@ -1,15 +1,20 @@
-from langchain.chains import ConversationalRetrievalChain
+from app.chat.chains.retrieval import StreamingConversationalRetrievalChain
 from app.chat.models import ChatArgs
 from app.chat.vector_stores.pinecone import build_retriever
 from app.chat.llms.chatopenai import build_llm
 from app.chat.memories.sql_memory import build_memory
+from langchain.chat_models import ChatOpenAI
 
 
 def build_chat(chat_args: ChatArgs):
     retriever = build_retriever(chat_args)
     llm = build_llm(chat_args)
+    condense_question_llm = ChatOpenAI(streaming=False)
     memory = build_memory(chat_args)
 
-    return ConversationalRetrievalChain.from_llm(
-        llm=llm, memory=memory, retriever=retriever
+    return StreamingConversationalRetrievalChain.from_llm(
+        llm=llm,
+        condense_question_llm=condense_question_llm,
+        memory=memory,
+        retriever=retriever,
     )
--- test.py
+++ test.py
@@ -1,28 +1,10 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.chains import LLMChain
-from langchain.callbacks.base import BaseCallbackHandler
 from dotenv import load_dotenv
-from queue import Queue
-from threading import Thread
-import warnings
+
 
 load_dotenv()
-warnings.filterwarnings("ignore")
-
-
-class StreamingHandler(BaseCallbackHandler):
-    def __init__(self, queue):
-        self.queue = queue
-
-    def on_llm_new_token(self, token, **kwargs):
-        self.queue.put(token)
-
-    def on_llm_end(self, response, **kwargs):
-        self.queue.put(None)
-
-    def on_llm_error(self, error, **kwargs):
-        self.queue.put(None)
 
 
 chat = ChatOpenAI(streaming=True)
@@ -30,22 +12,8 @@ chat = ChatOpenAI(streaming=True)
 prompt = ChatPromptTemplate.from_messages([("human", "{content}")])
 
 
-class StreamingChain(LLMChain):
-
-    def stream(self, input):
-        queue = Queue()
-        handler = StreamingHandler(queue)
-
-        def task():
-            self(input, callbacks=[handler])
-
-        Thread(target=task).start()
-
-        while True:
-            token = queue.get()
-            if token is None:
-                break
-            yield token
+class StreamingChain(StreamableChain, LLMChain):
+    pass
 
 
 chain = StreamingChain(llm=chat, prompt=prompt)

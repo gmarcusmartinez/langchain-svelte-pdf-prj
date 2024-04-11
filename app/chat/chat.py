fd16e411f5947661070c5bb6188e095c60e51387
Marcus Martinez <gmarcusmartinez@gmail.com>
2024-04-11 13:02:42 +0200

    begin component maps for randomizing llms and vector stores

--- app/chat/chat.py
+++ app/chat/chat.py
@@ -1,13 +1,32 @@
 from app.chat.chains.retrieval import StreamingConversationalRetrievalChain
 from app.chat.models import ChatArgs
-from app.chat.vector_stores.pinecone import build_retriever
+from app.chat.vector_stores import retriever_map
 from app.chat.llms.chatopenai import build_llm
 from app.chat.memories.sql_memory import build_memory
 from langchain.chat_models import ChatOpenAI
+from app.web.api import set_conversation_components, get_conversation_components
+import random
 
 
 def build_chat(chat_args: ChatArgs):
-    retriever = build_retriever(chat_args)
+    components = get_conversation_components(chat_args.conversation_id)
+    previous_retriever = components["retriever"]
+
+    retriever = None
+    if previous_retriever:
+        build_retriever = retriever_map[previous_retriever]
+        retriever = build_retriever(chat_args)
+    else:
+        random_retriever_name = random.choice(list(retriever_map.keys()))
+        build_retriever = retriever_map[random_retriever_name]
+        retriever = build_retriever(chat_args)
+        set_conversation_components(
+            conversation_id=chat_args.conversation_id,
+            llm="",
+            memory="",
+            retriever=random_retriever_name,
+        )
+
     llm = build_llm(chat_args)
     condense_question_llm = ChatOpenAI(streaming=False)
     memory = build_memory(chat_args)

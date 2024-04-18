baea0b57f45874f747378ed2f458060cb31f08a6
Marcus Martinez <gmarcusmartinez@gmail.com>
2024-04-18 11:49:14 +0200

    random_component_by_score function implemented

--- app/chat/redis.py
+++ app/chat/redis.py
@@ -1,4 +1,4 @@
 import os
 import redis
 
-client = redis.Redis.from_url(os.environ["REDIS_URL"], decode_responses=True)
+client = redis.Redis.from_url(os.environ["REDIS_URI"], decode_responses=True)
